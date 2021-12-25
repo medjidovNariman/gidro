02b33125b0005fe9fc40a0a2a0c36221dbbfd331
nariman <nariman.miedzhidov@mail.ru>
2021-12-25 09:51:39 +0300

    banner-section

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btn-next"><img src="images/arrow-right.svg" alt=""></button>'
+  });
+})
